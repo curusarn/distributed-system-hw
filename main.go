40a3d561587d8b338673dbe1e06d11832be560ed
Simon Let <simon.a.let@gmail.com>
2019-01-06 01:47:18 +0100

    join, leave, heartbeat

--- main.go
+++ main.go
@@ -3,59 +3,88 @@ package main
 import (
     "fmt"
     "time"
+    "net"
+    "os"
+    "io"
+    "log"
+    "strconv"
     flag "github.com/spf13/pflag"
     node "github.com/curusarn/distributed-system-hw/node"
 )
 
-var N *node.Node
+var logFile string = "dsv.log"
 
 func main() {
-    var ip string
-    flag.StringVar(&ip, "ip", "127.0.0.1",
-                   "Listenning IP address of the node")
-    var port int
-    flag.IntVar(&port, "port", 42000,
-                "Listenning port of the node")
     var initCluster bool
     flag.BoolVar(&initCluster, "init-cluster", false,
-                 "Node will initialize a cluster. (takes precedence over join options)")
+                 "Node will initialize a cluster. (takes precedence over join option)")
 
-    var joinIp string
-    flag.StringVar(&joinIp, "join-ip", "",
-                   "Join cluster on this IP address")
-    var joinPort int
-    flag.IntVar(&joinPort, "join-port", -1,
-                "Join cluster on this port")
+    var join string
+    flag.StringVar(&join, "join", "",
+                   "Join cluster on this IP:port")
+
+    var xxx bool
+    flag.BoolVar(&xxx, "xxx", false,
+                 "Node will do shit")
     flag.Parse()
 
-    n := node.New(ip, port)
+    if flag.NArg() != 1 {
+        flag.Usage()
+        os.Exit(1)
+    }
+
+    port, err := strconv.Atoi(flag.Args()[0])
+    if err != nil {
+        fmt.Print("Port has to be a number")
+        flag.Usage()
+        os.Exit(1)
+    }
+
+    f, err := os.Open("dsv.log")
+    if err != nil {
+        // fatal
+    }
+    mw := io.MultiWriter(os.Stdout, f)
+    log.SetOutput(mw)
+    log.Print("-- log init --")
+    log.Print(time.Now())
+
+    n := node.NewNode(GetMyIP(), port)
+    n.Print()
     if initCluster {
-        fmt.Println("Hello, init cluster!")
-        n.LeaderUid = n.Uid // set yourself as leader
-        n.NeighbourIp = n.Ip // set yourself as neighbour
-        n.NeighbourPort = n.Port
-        n.SendMsg("hey")
+        fmt.Println("Init cluster!")
+        n.InitCluster()
         n.Listen()
-        n.HeartbeatChecker()
-        for {
-            t := 2000 * time.Millisecond
-            time.Sleep(t)
-            //n.SendHeartbeat()
-        }
-    } else if joinIp != "" && joinPort != -1 {
-        fmt.Println("Hello, join cluster:")
-        fmt.Println(joinIp)
-        fmt.Println(joinPort)
-        n.NeighbourIp = joinIp // set yourself as neighbour
-        n.NeighbourPort = joinPort
-        for {
-            n.SendHeartbeat()
-            t := 2000 * time.Millisecond
-            time.Sleep(t)
+        n.DialNeighbour()
+        n.Run()
+    } else if join != "" {
+        fmt.Println("Join cluster:")
+        fmt.Println(join)
+
+        n.Listen()
+        n.Join(join)
+        if xxx {
+            n.RunLeave()
+        } else {
+            n.Run()
         }
     } else {
-        fmt.Println("Use --init-cluster or --join-xxx")
+        fmt.Println("Use --init-cluster or --join IP:port")
         flag.Usage()
     }
 
+
+}
+
+// Get preferred outbound ip of this machine
+func GetMyIP() net.IP {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+    return localAddr.IP
 }
--- node/node.go
+++ node/node.go
@@ -2,8 +2,8 @@ package node
 
 import (
     "fmt"
-    "strings"
     "strconv"
+    "strings"
     "net/rpc"
     "time"
     "log"
@@ -11,70 +11,52 @@ import (
     "net"
 )
 
-
 const heartbeatTimeout time.Duration = 3000 * time.Millisecond
 
-type NodeRpc struct {
-    Node *Node
-}
-
-type Node struct {
-    // const
+type NodeInfo struct {
     Uid int64
-    Ip string
-    Port int
-
-    LeaderUid int64 // leader == uid => I'm leader
-
-    // may use different types later
-    NeighbourIp string
-    NeighbourPort int
-
-    ParticipatingInElection bool
-
-    HeartbeatTs time.Time
-
-    X int
-    // topologyBroken bool ??
+    Addr string
 }
 
-func getUID(ip string, port int) int64 {
-    ipSlice := strings.Split(ip, ".")
-    var uid int64 = 0
-    for _, ipPart := range ipSlice {
-        ipPartInt, err := strconv.Atoi(ipPart)
-        if err != nil {
-            fmt.Print("The end is niegh!!!")
-        }
-        uid += int64(ipPartInt)
-        uid *= 1000
+type TwoNodeInfo struct {
+    NodeInfo
+    NewUid int64
+    NewAddr string
+}
+
+// NodeRpc
+
+type NodeRpc struct {
+    Node *node
+}
+
+func (r *NodeRpc) Heartbeat(arg bool, reply *bool) error {
+    r.Node.heartbeatTs = time.Now()
+    log.Print("got hb")
+    return nil
+}
+
+func (r *NodeRpc) Join(info NodeInfo, reply *NodeInfo) error {
+    log.Print("Recieved Join from ")
+    log.Print(info.Addr)
+    // what if I do not have a neighbour ?
+    reply.Uid = 0
+    reply.Addr = r.Node.neighbourAddr
+    r.Node.neighbourAddr = info.Addr
+    r.Node.DialNeighbour()
+    return nil
+}
+
+func (r *NodeRpc) Leave(info TwoNodeInfo, reply *bool) error {
+    log.Print("Recieved Leave from ")
+    log.Print(info.Addr)
+    // what if I do not have a neighbour ?
+    if info.Addr != r.Node.neighbourAddr {
+        r.Node.ForwardLeave(info)
+        return nil
     }
-    uid *= 1000
-    uid += int64(port)
-    fmt.Print("uid: ")
-    fmt.Println(uid)
-    return uid
-}
-
-func New(ip string, port int) Node {
-    uid := getUID(ip, port)
-    n := Node {
-        Uid: uid,
-        Ip: ip,
-        Port: port,
-        LeaderUid: -1,
-        NeighbourIp: "",
-        NeighbourPort: -1,
-        ParticipatingInElection: false,
-    }
-    return n
-}
-
-func (r *NodeRpc) RpcHeartbeat(arg bool, reply *bool) error {
-    r.Node.HeartbeatTs = time.Now()
-    r.Node.X = 1
-    fmt.Println("got hb at")
-    fmt.Println(r.Node.HeartbeatTs)
+    r.Node.neighbourAddr = info.NewAddr
+    r.Node.DialNeighbour()
     return nil
 }
 
@@ -82,26 +64,182 @@ func (r *NodeRpc) NonRpcFunc(arg bool) error {
     return nil
 }
 
-func (n Node) SendHeartbeat() {
-    client, err := rpc.DialHTTP("tcp", n.NeighbourIp + ":42420")
+
+func Watch(r *NodeRpc) {
+    for {
+        t := 2000 * time.Millisecond
+        time.Sleep(t)
+    }
+}
+
+// Node
+
+// do i need this ?
+type Node interface {
+    InitCluster()
+    Listen()
+}
+
+type node struct {
+    // const
+    uid int64
+    addr string // socket addres
+
+    leaderUid int64 // leader == uid => I'm leader
+
+    neighbourAddr string
+    neighbourRpc *rpc.Client
+
+    participatingInElection bool
+
+    heartbeatTs time.Time
+
+    // topologyBroken bool ??
+}
+
+func getUid(ip net.IP, port int) int64 {
+    var uid int64 = 0
+    for _, ipPart := range ip {
+        uid += int64(ipPart)
+        uid *= 1000
+    }
+    uid *= 1000
+    uid += int64(port)
+    fmt.Print("Node uid: ")
+    fmt.Println(uid)
+    return uid
+}
+
+func getIpPort(ip net.IP, port int) string {
+    return ip.String() + ":" + strconv.Itoa(port)
+}
+
+func NewNode(ip net.IP, port int) node {
+    n := node {
+        uid: getUid(ip, port),
+        addr: getIpPort(ip, port),
+    }
+    return n
+}
+
+func (n node) getNodeInfo() NodeInfo {
+    return NodeInfo {n.uid, n.addr}
+}
+
+func (n node) Print() {
+    log.Print("uid: " + strconv.Itoa(int(n.uid)))
+    log.Print("addr: " + n.addr)
+}
+
+func (n node) PrintState() {
+    log.Print("neigh: " + n.neighbourAddr)
+    log.Print("leader: " + strconv.Itoa(int(n.leaderUid)))
+}
+
+func (n *node) InitCluster() {
+    n.leaderUid = n.uid // set yourself as leader
+    n.neighbourAddr = n.addr // set yourself as neighbour
+}
+
+func (n *node) DialNeighbour() {
+    client, err := rpc.DialHTTP("tcp", n.neighbourAddr)
     if err != nil {
         log.Fatal("dialing:", err)
     }
-    // Synchronous call
-    var reply bool
-    err = client.Call("NodeRpc.RpcHeartbeat", true, &reply)
-    if err != nil {
-        log.Fatal("hb error:", err)
-    }
-    fmt.Println("hb sent at")
-    fmt.Println(time.Now())
+    n.neighbourRpc = client
 }
 
-func (n *Node) HeartbeatChecker() {
-    n.HeartbeatTs = time.Now() // init
+func (n *node) Join(addr string) {
+    // dial before call ... duh
+    client, err := rpc.DialHTTP("tcp", addr)
+    if err != nil {
+        log.Fatal("dialing:", err)
+    }
+
+    var reply NodeInfo
+    log.Print("Join ...")
+    err = client.Call("NodeRpc.Join", n.getNodeInfo(), &reply)
+    if err != nil {
+        log.Fatal("call error:", err)
+    }
+    log.Print("join atempt")
+
+    n.neighbourAddr = reply.Addr
+    n.DialNeighbour()
+
+    log.Print("joined")
+}
+
+func (n node) Leave() {
+    // Synchronous call
+    info := TwoNodeInfo {n.getNodeInfo(), 0, n.neighbourAddr}
+    var reply bool
+    log.Print("Leave ...")
+    err := n.neighbourRpc.Call("NodeRpc.Leave", info, &reply)
+    if err != nil {
+        log.Fatal("call error:", err)
+    }
+
+    n.neighbourAddr = ""
+    n.neighbourRpc = nil
+
+    log.Print("Left")
+}
+
+func (n node) ForwardLeave(info TwoNodeInfo) {
+    // Synchronous call
+    var reply bool
+    log.Print("ForwardLeave ...")
+    err := n.neighbourRpc.Call("NodeRpc.Leave", info, &reply)
+    if err != nil {
+        log.Fatal("call error:", err)
+    }
+    log.Print("ForwardLeave done")
+}
+
+func (n *node) Run() {
+    for {
+        for i := 0; i < 3; i++ {
+            log.Print("ready.")
+            n.SendHeartbeat()
+            time.Sleep(2000 * time.Millisecond)
+        }
+        n.PrintState()
+        time.Sleep(2000 * time.Millisecond)
+    }
+}
+
+func (n *node) RunLeave() {
+    for {
+        for i := 0; i < 5; i++ {
+            for i := 0; i < 3; i++ {
+                log.Print("ready.")
+                n.SendHeartbeat()
+                time.Sleep(2000 * time.Millisecond)
+            }
+            n.PrintState()
+            time.Sleep(2000 * time.Millisecond)
+        }
+        n.Leave()
+        return
+    }
+}
+
+func (n node) SendHeartbeat() {
+    // Synchronous call
+    var reply bool
+    err := n.neighbourRpc.Call("NodeRpc.Heartbeat", true, &reply)
+    if err != nil {
+        log.Fatal("call error:", err)
+    }
+    log.Print("hb sent")
+}
+
+func (n *node) HeartbeatChecker() {
+    n.heartbeatTs = time.Now() // init
     for {
         time.Sleep(heartbeatTimeout)
-        ts := n.HeartbeatTs
+        ts := n.heartbeatTs
         fmt.Println("checking hb expiration at")
         fmt.Println(time.Now())
         fmt.Println("> last hb at")
@@ -115,35 +253,25 @@ func (n *Node) HeartbeatChecker() {
     }
 }
 
-func Watch(r *NodeRpc) {
-    for {
-        t := 2000 * time.Millisecond
-        time.Sleep(t)
-        fmt.Println("X:")
-        fmt.Println(r.Node.X)
-    }
-}
-
-func (n *Node) Listen() {
+func (n *node) Listen() {
     r := &NodeRpc {
         Node: n,
     }
-    r.Node.X = 0
     rpc.Register(r)
     rpc.HandleHTTP()
-    l, e := net.Listen("tcp", ":42420")
+    port := ":" + strings.Split(n.addr, ":")[1]
+    l, e := net.Listen("tcp", port)
     if e != nil {
         log.Fatal("listen error:", e)
         fmt.Println("listen error:", e)
     }
     go http.Serve(l, nil)
-    go Watch(r)
 }
 
-func (n Node) RepairTopology() {
+func (n node) RepairTopology() {
     fmt.Println("RepairTopology")
 }
 
-func (n Node) SendMsg(msg string) bool {
+func (n node) SendMsg(msg string) bool {
     return true
 }
